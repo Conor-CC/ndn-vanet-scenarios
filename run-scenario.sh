1ab2043d07417f7fb60172b52b5f6fe0cd8ea1fc
ConorClery <cleryc@tcd.ie>
2020-02-04 10:53:56 +0000

    Script for easily configuring and running the scenario based on sumo trace files

---
+++ run-scenario.sh
@@ -0,0 +1,1 @@
+./waf --run "ndn-v2v-simple --producer-count=45 --consumer-count=45 --num-nodes=45 --simulation-length=429 --trace-file=$SUMO_PROJECTS_ROOT/greystones-roundabouts-dense/ns2-trace.tcl"
