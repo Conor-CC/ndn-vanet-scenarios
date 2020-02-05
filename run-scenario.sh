498a0f7258805c9dbe477ba73ef04ed77090c3d8
ConorClery <cleryc@tcd.ie>
2020-02-05 09:21:44 +0000

    Updated gitignore and removed localised script

--- run-scenario.sh
+++
@@ -1,1 +0,0 @@
-./waf --run "ndn-v2v-simple --producer-count=45 --consumer-count=45 --num-nodes=45 --simulation-length=429 --trace-file=$SUMO_PROJECTS_ROOT/greystones-roundabouts-dense/ns2-trace.tcl"
